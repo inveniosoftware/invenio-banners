f28744f7b307091094a24cc16e5c0547f5b9893d
Christoph Ladurner <christoph.ladurner@tugraz.at>
2025-10-23 08:45:15 +0200

    compatibility(312): distutils removed
    * distuils has been removed with python3.12. strtobool is the only
      function with a relatively clear specification. therefore
      reimplemented

--- invenio_banners/utils.py
+++ invenio_banners/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020-2023 CERN.
+# Copyright (C) 2025 Graz University of Technology.
 #
 # Invenio-Banners is free software; you can redistribute it and/or modify it
 # under the terms of the MIT License; see LICENSE file for more details.
@@ -28,3 +29,24 @@ def style_category(category):
     else:
         style_class = style_class.format("info")
     return style_class
+
+
+def strtobool(value):
+    """String to bool.
+
+    since python3.12 removed distutils.util and the function is simple it has
+    been reimplemented
+
+    Convert a string representation of truth to true (1) or false (0).
+
+    True values are y, yes, t, true, on and 1; false values are n, no, f, false,
+    off and 0. Raises ValueError if val is anything else.
+    """
+    v = str(value)
+    if v.lower() in ["y", "yes", "t", "true", "on", "1"]:
+        return True
+    elif v.lower() in ["n", "no", "f", "false", "off", "0"]:
+        return False
+    else:
+        msg = f"value: {value} is not of y, yes, t, true, on, 1 or n, no, f, false, off, 0"
+        raise ValueError(msg)
