eaeef64455a9946d07222730f5fff4fbe20c88fa
Christoph Ladurner <christoph.ladurner@tugraz.at>
2026-01-30 12:35:10 +0100

    release: v6.0.0

--- invenio_banners/__init__.py
+++ invenio_banners/__init__.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # Copyright (C) 2020-2024 CERN.
-# Copyright (C) 2024-2025 Graz University of Technology.
+# Copyright (C) 2024-2026 Graz University of Technology.
 # Copyright (C) 2025      KTH Royal Institute of Technology.
 #
 # Invenio-Banners is free software; you can redistribute it and/or modify it
@@ -11,6 +11,6 @@
 
 from .ext import InvenioBanners
 
-__version__ = "5.2.1"
+__version__ = "6.0.0"
 
 __all__ = ("__version__", "InvenioBanners")
